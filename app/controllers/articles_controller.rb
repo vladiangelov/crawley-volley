d2dbdb4b71469b68affc13fddcb35739a739c9d3
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-02-03 19:09:55 +0000

    Added edit/delete for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,24 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article.id)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article.id)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.delete
+
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
