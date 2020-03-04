ef2ed8df5ed9327296f70b93822ac56bcc1742b7
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-03-04 22:10:13 +0000

    Added a map for venues and some small fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :venues, :teams, :contact]
+  skip_before_action :authenticate_user!, only: [:home, :venues, :teams, :contact, :about]
 
   def home
   end
 
   def venues
+    @markers = [{ lat: 51.133630, lng: -0.033350 }, # Imberhorne School
+                { lat: 51.117859, lng: -0.209684 }, # Ifield CC
+                { lat: 51.122058, lng: -0.176779 }] # Northgate playing field
   end
 
   def teams
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'venues', to: 'pages#venues', as: :venues
   get 'teams', to: 'pages#teams', as: :teams
   get 'contact', to: 'pages#contact', as: :contact
+  get 'about', to: 'pages#about', as: :about
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
