e3ab0ce9bdb9c03876f95d25aaef00a4e5a5d26a
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-03-04 20:29:06 +0000

    added a few static pages, fixed the articles CRUD (logged in users only can CUD)  and added logout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :venues, :teams, :contact]
 
   def home
+  end
+
+  def venues
+  end
+
+  def teams
+  end
+
+  def contact
   end
 end
