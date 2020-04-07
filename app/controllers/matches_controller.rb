13e795d4889cc279fd0d1b3928146a060d420871
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-04-07 20:52:51 +0100

    Added create a match for user

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,4 +2,21 @@ class MatchesController < ApplicationController
   def index
     @matches = Match.all.order(date: :desc)
   end
+
+  def new
+    @match = Match.new
+  end
+
+  def create
+    @match = Match.new(match_params)
+    @match.save
+
+    redirect_to matches_path
+  end
+
+  private
+
+  def match_params
+    params.require(:match).permit(:team, :home_team, :away_team, :home_sets, :away_sets, :score, :league, :venue, :address, :date)
+  end
 end
