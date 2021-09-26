ffa71b626ee7d20c5d0335eba89329c2eeec50cd
Vladi Angelov <vladislav_angelov@yahoo.com>
2021-09-26 19:15:06 +0100

    feat: Add option to manage venues on the admin page

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,40 @@
+class VenuesController < ApplicationController
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.save
+
+    redirect_to venues_path
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+    @venue.update(venue_params)
+
+    redirect_to venues_path
+  end
+
+  def destroy
+    @venue = Venue.find(params[:id])
+    @venue.delete
+
+    redirect_to venues_path
+  end
+
+  def admin
+    @venues = Venue.all
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :training_day, :training_time, :additional_info, :lat, :long)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get 'admin', to: 'pages#admin', as: :admin
   resources :articles
   resources :matches
+  resources :venues, only: [:new, :create, :edit, :update, :destroy]
+  get 'admin_venues', to: 'venues#admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
