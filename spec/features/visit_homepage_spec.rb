6804cb9d6aac9f17fb34c5c4de049fd868a8c998
Vladi Angelov <vladislav_angelov@yahoo.com>
2021-11-06 12:16:07 +0000

    revering rubocop linting tests

--- spec/features/visit_homepage_spec.rb
+++ spec/features/visit_homepage_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Visiting the homepage", type: :feature do
-  scenario "shows the navbar with all links" do
+RSpec.describe 'Visiting the homepage', type: :feature do
+  scenario 'shows the navbar with all links' do
     visit about_path
-    expect(page).to have_content("ABOUT")
+    expect(page).to have_content('ABOUT')
   end
 end
