36fb0371f7d11707a0326ebde8d95892ff6b6570
vladiangelov <36208690+vladiangelov@users.noreply.github.com>
2021-11-07 17:12:11 +0000

    test: Add login feature test & refactor (#51)
    * test: Add login feature test & refactor
    
    * refactor: ignore some block length methods rubocop

--- spec/features/visit_homepage_spec.rb
+++ spec/features/visit_homepage_spec.rb
@@ -3,8 +3,61 @@
 require 'rails_helper'
 
 RSpec.describe 'Visiting the homepage', type: :feature do
+  before(:example) do
+    Player.create!(
+      first_name: 'Joe',
+      last_name: 'Bloggs',
+      team: 'Men',
+      position: 'Outside hitter',
+      shirt_number: 1
+    )
+
+    Match.create!(
+      team: 'Men',
+      home_team: 'Team A',
+      away_team: 'Team B',
+      home_sets: 3,
+      away_sets: 2,
+      venue: 'A random venue',
+      date: DateTime.now,
+      score: '25:20 25:20 22:25 21:25 10:15'
+    )
+
+    @user = User.create!(
+      email: 'test@crawleysparks.co.uk',
+      password: 'blabla'
+    )
+
+    Article.create!(
+      title: 'Sample title',
+      content: 'Sample content',
+      user: @user
+    )
+  end
+
   scenario 'shows the navbar with all links' do
-    visit about_path
-    expect(page).to have_content('ABOUT')
+    visit root_path
+    expect(page).to have_content('ABOUT VENUES NEWS TEAMS MATCHES CONTACT')
+  end
+
+  scenario 'shows the login button for users' do
+    visit root_path
+    expect(page).to have_content('Member Login')
+  end
+
+  scenario 'allows members to login' do
+    visit root_path
+    within('.member-login') do
+      click_link 'Member Login'
+    end
+    expect(page).to have_content('Log in')
+    within('.new_user') do
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+    end
+    click_button 'Log in'
+
+    expect(page).to have_content('Admin Page')
+    expect(page).to have_content('Sign Out')
   end
 end
